75396a66b9835b5ca50f56ed92046f107c513def
dev-4xt <chuhoangsam21@gmail.com>
2023-03-29 06:13:34 +0700

    this is some first file of program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+namespace Project
+{
+    class MainPrpject
+    {
+        public int threadCount{get;set;}
+        public static async Task SentHTTPRequest(int threadCount)
+        {
+            Task[] tasksThread = new Task[threadCount];
+            for(int i = 0;i<threadCount;i++)
+            {
+                tasksThread[i]= Task.Run(()=>{
+                    using(HttpClient client = new HttpClient())
+                    {
+                        HttpRequestMessage request = new HttpRequestMessage()
+                        {
+                            Headers ={
+
+                            }
+                        };
+                    }
+                });
+                await Task.WhenAll(tasksThread);
+            }
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
